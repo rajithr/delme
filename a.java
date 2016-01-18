c0bfd8a8ecc3909ada769438776e5aeb416f1faf
rajithr <r.rajith@gmail.com>
2016-01-18 22:08:06 +0800

    Update a.java

--- a.java
+++ a.java
@@ -1,5 +1,5 @@
 public class a{
-  public static void main throws Exception (String [] args)
+  public static void main(String [] args) throws Exception
   {
     for(int i=10;i > 0;i--)
       System.out.println("Hello");
